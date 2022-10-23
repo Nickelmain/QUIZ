02b13917f9fbfaf33f6eededac9adecfd4af76f9
Nikita <114959529+Nickelmain@users.noreply.github.com>
2022-10-23 22:48:31 +0100

    24.10.2022

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using quiz;
+
+
+var p1 = new Question();
+p1.Id = 1;
+p1.Category = 100;
+p1.Content = "Jaka jest stolica Ukrainy?";
+p1.Answer1 = new Answer();
+p1.Answer1.Id = 1;
+p1.Answer1.Content = "Kijów";
+p1.Answer1.IsCorrect = true;
+p1.Answer2 = new Answer();
+p1.Answer2.Id = 2;
+p1.Answer2.Content = "Lwów";
+p1.Answer2.IsCorrect = false;
+p1.Answer3 = new Answer();
+p1.Answer3.Id = 3;
+p1.Answer3.Content = "Charków";
+p1.Answer3.IsCorrect = false;
+p1.Answer4 = new Answer();
+p1.Answer4.Id = 4;
+p1.Answer4.Content = "Kijów";
+p1.Answer4.IsCorrect = false;    
---
+++ Question.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quiz
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public int Category { get; set; }
+        public string Content { get; set; }
+        public Answer Answer1 { get; set; }
+        public Answer Answer2 { get; set; }
+        public Answer Answer3 { get; set; }
+        public Answer Answer4 { get; set; }
+
+    }
+}
